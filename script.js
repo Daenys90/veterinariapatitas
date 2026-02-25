b8e0f2daf82ff9e7a167278e610abd8dc2c4638d
Daenys90 <denisse.vidis.stelos@gmail.com>
2026-02-24 23:44:02 -0300

    segundo commit

--- script.js
+++ script.js
@@ -10,6 +10,17 @@ class Mascota {
     }
   }
 
+//subi esta funcion para que se vea correctamente
+  
+function mostrarPacientes(){
+    const lista = document.getElementById("lista-pacientes");
+lista.innerHTML = usuarioAutenticado.listaMascotas.map(mascota =>
+    `<li class="list-group-item">
+    Paciente: ${mascota.nombre} - Tutor: ${mascota.tutor} - Evolución médica: ${mascota.evolucionMedica}</li>
+    `).join('');
+}
+
+
 //clase vet
 class Veterinario{
 constructor(nombreUsuario, contrasena){
@@ -25,10 +36,66 @@ agregarMascota(mascota){
 }
 
 //probando
-const veterinario01 = new Veterinario("josesolis", "perro1235" );
+const veterinario01 = new Veterinario("josesolis", "perro12345" );
 
 const paciente01 = new Mascota("Pelusa", "Juan Soto", "Control anual");
 
 veterinario01.agregarMascota(paciente01);
 
 console.log(veterinario01.listaMascotas);
+
+// crear dos instancias, ya hice una asi que dejaré otra
+const veterinario02 = new Veterinario("anasocias", "lunita1995");
+
+const listaVeterinarios = [veterinario01, veterinario02];
+
+//lave vacía, aquí no entendí mucho esto, tuve que buscar qué me estaban pidiendo
+let usuarioAutenticado = null;
+
+//iniciar sesion
+function iniciarSesion(evento){
+evento.preventDefault(); //esto lo tuve que agregar porque no se cargaba bien la segunda pantalla (la de registro de mascotas)
+const usuarioIngresado = document.getElementById("inputUsuario").value;
+const contrasenaIngresada = document.getElementById("inputPassword").value;
+
+const usuarioEncontrado = listaVeterinarios.find(vet => vet.nombreUsuario === usuarioIngresado && vet.contrasena === contrasenaIngresada
+);
+
+if (usuarioEncontrado){
+    usuarioAutenticado = usuarioEncontrado;
+    alert("Bienvenido, " + usuarioAutenticado.nombreUsuario);
+
+//aparece la nueva pantalla de registro de pacientes
+    document.getElementById("seccion-formulario").style.display = "none";
+    document.getElementById("seccion-registro").style.display = "block";
+
+//mostrar pacientes del doctor
+mostrarPacientes();
+
+//si no coinciden
+} else {
+    alert("Usuaurio no encontrado o contraseña incorrecta");
+}
+}
+
+// conectar form 
+const formulario = document.querySelector("form");
+formulario.addEventListener("submit", iniciarSesion);
+
+//registro mascota
+
+const formularioMascota = document.getElementById("formulario-mascotas");
+
+formularioMascota.addEventListener("submit", (evento) => {
+    evento.preventDefault();
+
+    const nombre = document.getElementById("nombreMascota").value;
+    const tutor = document.getElementById("tutorMascota").value;
+    const evolucion = document.getElementById("evolucionMascota").value;
+
+    const nuevaMascota = new Mascota(nombre, tutor, evolucion);
+
+    usuarioAutenticado.agregarMascota(nuevaMascota);
+    alert("Nueva mascota agregada con éxito");
+    mostrarPacientes();
+});
